44059a2901c6ada640b321d5e9e292464ed3fb51
Ayush Vahane <ayushrao9981@gmail.com>
2025-03-12 16:46:29 +0530

    First commit

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -15,6 +15,7 @@ public class HotelReservationSystem {
     private static final String password = "Aman@123";
 
     public static void main (String [] args){
+        System.out.println("Hello");
 
     }
 }
