7d5cf7d14eac690573ae3334f2816dc5996525a6
Ayush Vahane <ayushrao9981@gmail.com>
2025-03-12 16:43:34 +0530

    First commit

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -1,4 +1,19 @@
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Scanner;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
 public class HotelReservationSystem {
+
+    private static final String url = "jdbc:mysql://localhost:3306/hotel_database";
+
+    private static final String username = "root";
+
+    private static final String password = "Aman@123";
+
     public static void main (String [] args){
 
     }
